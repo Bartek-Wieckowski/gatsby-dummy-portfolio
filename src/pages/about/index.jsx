434b88e401332391c84686390868d06a93c994d9
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-08 14:08:19 +0200

    created content of about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+import imagesPath from "../../images/images";
+
+
+export default function About() {
+  return (
+    <Layout>
+      <section className="about section " id="about">
+        <div className="about__container grid">
+          <div className="about__img">
+            <img src={imagesPath.man} alt="" />
+          </div>
+          <div>
+            <h2 className="about__subtitle">the best frontend devs</h2>
+            <p className="about__text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate cum expedita quo culpa
+              tempora, assumenda, quis fugiat ut voluptates soluta, aut earum nemo recusandae cumque
+              perferendis! Recusandae alias accusamus atque.
+            </p>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
