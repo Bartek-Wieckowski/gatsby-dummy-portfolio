e8738ae80c80f146b1beba3f2e98e77774c81633
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-09 13:19:15 +0200

    created funny footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <p class="footer__title">theBart practice Gatsby</p>
+    </footer>
+  );
+}
