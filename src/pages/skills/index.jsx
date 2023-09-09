504e02dfb1f86a90a4da93c0ea91a0b86e927dab
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-09 11:52:22 +0200

    created state of skill list pass props to progress bar component

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
+import ProgressBar from "../../components/progress-bar/ProgressBar";
 
 export default function Skills() {
+  const [progressBarValue, setProgressBarValue] = useState([
+    {
+      id: "html",
+      title:"HTML5",
+      percent: 0,
+      targetPercent: 85,
+      img: "/images/file-html-fill.svg",
+    },
+    {
+      id: "css",
+      title:"CSS3",
+      percent: 0,
+      targetPercent: 93,
+      img: "/images/file-css-fill.svg",
+    },
+    {
+      id: "js",
+      title:"JavaScript",
+      percent: 0,
+      targetPercent: 95,
+      img: "/images/file-js-fill.svg",
+    },
+    {
+      id: "ui",
+      title:"UI",
+      percent: 0,
+      targetPercent: 90,
+      img: "/images/pen-fill.svg",
+    },
+  ]);
+  useEffect(() => {
+    const intervals = progressBarValue.map((pbv) => {
+      return setInterval(() => {
+        if (pbv.percent < pbv.targetPercent) {
+          setProgressBarValue((prevValues) =>
+            prevValues.map((pv) => (pv.id === pbv.id ? { ...pv, percent: pv.percent + 1 } : pv))
+          );
+        }
+      });
+    }, 100);
+    return () => {
+      intervals.forEach((int) => clearInterval(int));
+    };
+  }, [progressBarValue]);
+
   return (
     <Layout>
       <section className="skills section" id="skills">
@@ -13,46 +59,9 @@ export default function Skills() {
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit optio id vero amet, alias
               architecto consectetur error eum eaque sit.
             </p>
-            <div className="skills__data">
-              <div className="skills__names">
-                <img src="/images/file-html-fill.svg" alt="" />
-                <span className="skills__name">HTML5</span>
-              </div>
-              <div className="skills__bar skills__html"></div>
-              <div>
-                <span className="skills__percentage">85%</span>
-              </div>
-            </div>
-            <div className="skills__data">
-              <div className="skills__names">
-                <img src="/images/file-css-fill.svg" alt="" />
-                <span className="skills__name">CSS3</span>
-              </div>
-              <div className="skills__bar skills__css"></div>
-              <div>
-                <span className="skills__percentage">93%</span>
-              </div>
-            </div>
-            <div className="skills__data">
-              <div className="skills__names">
-                <img src="/images/file-js-fill.svg" alt="" />
-                <span className="skills__name">JAVASCRIPT</span>
-              </div>
-              <div className="skills__bar skills__js"></div>
-              <div>
-                <span className="skills__percentage">95%</span>
-              </div>
-            </div>
-            <div className="skills__data">
-              <div className="skills__names">
-                <img src="/images/pen-fill.svg" alt="" />
-                <span className="skills__name">UI</span>
-              </div>
-              <div className="skills__bar skills__ux"></div>
-              <div>
-                <span className="skills__percentage">90%</span>
-              </div>
-            </div>
+            {progressBarValue.map((progressBar) => (
+              <ProgressBar key={progressBar.id} {...progressBar} />
+            ))}
           </div>
           <div>
             <img src="/images/skill.jpg" alt="" className="skills__img" />
