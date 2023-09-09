4c03e62e424dc5b5a89fe161fcedccd6e293e828
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-09 13:20:04 +0200

    used Footer outside Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
 import "./layout.css";
+import Footer from "../footer/Footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       <main className="main">{children}</main>
+      <Footer />
     </>
   );
 }
