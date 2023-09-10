9370e6802c843be69f0f09b4e715ec2f9985d9fc
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-10 12:50:17 +0200

    changed content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="footer">
-      <p class="footer__title">theBart practice Gatsby</p>
+    <footer className="footer">
+      <p className="footer__title">theBart practice Gatsby</p>
     </footer>
   );
 }
