efffb1b7cf5a231414ede969f2cea0986d7441dd
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-11 11:41:20 +0200

    created form with react hook from

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
 import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm();
+
   const slideLeftEffect = {
     initial: { x: "-100%" },
     animate: { x: 0 },
     exit: { x: "100%" },
+  };
+
+  const onSubmit = (data) => {
+    console.log("send");
+
+    reset();
   };
 
   return (
@@ -19,11 +34,33 @@ export default function Contact() {
         exit="exit"
       >
         <div className="contact__container grid">
-          <form action="" className="contact__form">
-            <input type="text" placeholder="Name" className="contact__input" />
-            <input type="mail" placeholder="Email" className="contact__input" />
-            <textarea name="" id="" cols="0" rows="10" className="contact__input"></textarea>
-            <input type="button" value="Send" className="contact__button button" />
+          <form action="" className="contact__form" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder="Name"
+              className={`${!errors.name ? "contact__input mar-bott" : "contact__input"}`}
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && <p className="errorMsg">{`${errors.name.message}`}</p>}
+            <input
+              type="email"
+              placeholder="Email"
+              className={`${!errors.email ? "contact__input mar-bott" : "contact__input"}`}
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && <p className="errorMsg">{`${errors.email.message}`}</p>}
+            <textarea
+              name=""
+              id=""
+              cols="0"
+              rows="10"
+              className={`${!errors.message ? "contact__input mar-bott" : "contact__input"}`}
+              {...register("message", { required: "Message is required" })}
+            ></textarea>
+            {errors.message && <p className="errorMsg">{`${errors.message.message}`}</p>}
+            <button type="submit" className="contact__button button" disabled={isSubmitting}>
+              Send
+            </button>
           </form>
         </div>
       </motion.section>
